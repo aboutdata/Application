e1dfcfbfa5c18181e3dab6b6355159994a8945af
一名清官 <aboutdata@vip.qq.com>
2015-07-15 09:48:03 +0800

    集成ContextLoaderListener，而不是import javax.servlet.ServletContextListener;

--- src/main/java/com/anhao/web/controller/ServletListener.java
+++ src/main/java/com/anhao/web/controller/ServletListener.java
@@ -3,13 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.anhao.web.controller;
+
+import com.anhao.domain.Admin;
+import com.anhao.service.AdminService;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.annotation.WebListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
 
 /**
  * 时间: 2015-7-10
+ *
+ * @ 集成ContextLoaderListener，而不是import javax.servlet.ServletContextListener;
  * @author aboutdata
  */
-public class ServletListener {
+@WebListener
+public class ServletListener extends ContextLoaderListener {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        long start = System.currentTimeMillis();
+        logger.info("contextInitialized start ...");
+        //获取spring上下文
+        ApplicationContext applicationContext = getCurrentWebApplicationContext();
+        AdminService adminService = (AdminService) applicationContext.getBean("adminServiceImpl");
+        for (Admin admin : adminService.findAll()) {
+            logger.info("admin {}", admin);
+        }
+        logger.info("contextInitialized {}", System.currentTimeMillis() - start);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
 
 }
